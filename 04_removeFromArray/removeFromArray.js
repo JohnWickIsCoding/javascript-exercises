8978a94138d3bd12933091d871dc44bc6b761309
John Wick <johnwickiscoding@gmail.com>
2024-09-29 12:16:42 +0530

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    let removeCounter = 0
+
+    for (const arg of theArgs) {
+        for (i = 0; i < array.length; i++) {
+            if(array[i] === arg) {
+                array.splice(i, 1)
+                array.unshift("")
+                removeCounter++
+            }
+        }
+    }
+
+    for (i = 0; i < removeCounter; i++) {
+        array.shift()
+    }
+    
+    return array
 };
 
 // Do not edit below this line
